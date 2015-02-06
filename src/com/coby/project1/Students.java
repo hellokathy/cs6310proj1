060d5807c3b62ee867f68df86dc831134d545ee3
Coby Lu <lucoby@gmail.com>
2015-02-06 15:17:55 -0500

    Students test stuff

--- src/com/coby/project1/Students.java
+++ src/com/coby/project1/Students.java
@@ -1,13 +1,34 @@
 package com.coby.project1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Students {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		Scanner students = new Scanner(new File("student_schedule.txt"));
+		String s;
+		
+		while (students.hasNextLine()) {
+			s = students.nextLine();
+			if (s.matches("\\s+\\d\\..*")) {
+				System.out.println(s);
+				String[] tokens = s.split("[ .]+");
+				for(String t:tokens) {
+					if (t.matches(".*\\d.*"))
+					System.out.println(Integer.parseInt(t));
+					
+				}
+			}
+			
+		}
+		students.close();
 	}
 
 }
